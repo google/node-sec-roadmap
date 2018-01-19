35a18da880fea00dacf449f2e991200548d625a8
Mike Samuel <mikesamuel@gmail.com>
2018-01-19 13:35:41 -0500

    use Standard style for sh`...` definition

--- chapter-7/examples/sh/test/test.js
+++ chapter-7/examples/sh/test/test.js
@@ -15,236 +15,243 @@
  * limitations under the License.
  */
 
-const expect = require("chai").expect;
-const sh = require("../index");
+const expect = require('chai').expect
+const {describe, it} = require('mocha')
+const sh = require('../index')
 
 /**
  * Feeds chunks to the lexer and concatenates contexts.
  * Tests that the lexer ends in a valid end state and
- * appends "_ERR_" as an end state if not.
+ * appends '_ERR_' as an end state if not.
  */
-function tokens(...chunks) {
-    let lexer = sh.makeLexer();
-    let out = [];
-    for (let i = 0, n = chunks.length; i < n; ++i) {
-        out.push(lexer(chunks[i])[0] || '_');
-    }
-    try {
-        lexer(null);
-    } catch (e) {
-        out.push('_ERR_');
-    }
-    return out.join(',');
+function tokens (...chunks) {
+  let lexer = sh.makeLexer()
+  let out = []
+  for (let i = 0, n = chunks.length; i < n; ++i) {
+    out.push(lexer(chunks[i])[0] || '_')
+  }
+  try {
+    lexer(null)
+  } catch (e) {
+    out.push('_ERR_')
+  }
+  return out.join(',')
 }
 
 // Unwrap an ShFragment, failing if the result is not one.
-function uw(x) {
-    if (x instanceof sh.ShFragment) {
-        return String(x);
-    }
-    throw new Error('Expected ShFragment not ' + JSON.stringify(x));
+function uw (x) {
+  if (x instanceof sh.ShFragment) {
+    return String(x)
+  }
+  throw new Error('Expected ShFragment not ' + JSON.stringify(x))
 }
 
 // Run a test multiply  to exercise the memoizing code.
-function runShTest(golden, test) {
-    for (let i = 3; --i >= 0;) {
-        if (golden === '_ERR_') {
-            expect(test).to.throw();
-        } else {
-            let result = uw(test());
-            expect(golden).to.equal(result);
-        }
+function runShTest (golden, test) {
+  for (let i = 3; --i >= 0;) {
+    if (golden === '_ERR_') {
+      expect(test).to.throw()
+    } else {
+      let result = uw(test())
+      expect(result).to.equal(golden)
     }
+  }
 }
 
 describe('sh template tags', () => {
-    describe('lexer', () => {
-        it('empty string', () => {
-            expect('_').to.equal(tokens(''));
-        });
-        it('word', () => {
-            expect('_').to.equal(tokens('foo'));
-        });
-        it('words', () => {
-            expect('_').to.equal(tokens('foo bar baz'));
-        });
-        it('words split', () => {
-            expect('_,_,_').to.equal(tokens('foo bar', ' ', 'baz'));
-        });
-        it('parens', () => {
-            expect('_').to.equal(tokens('foo (bar) baz'));
-        });
-        it('parens split', () => {
-            expect('_,_,(,_,_,_').to.equal(
-              tokens('foo', ', ', '(bar', ')', ' ', 'baz'));
-        });
-        it('parens hanging split', () => {
-            expect('_,_,(,(,(,_ERR_').to.equal(
-              tokens('foo', ', ', '(bar', ' ', 'baz'));
-        });
-        it('quotes embed subshell', () => {
-            expect('",$(,_').to.equal(
-              tokens(' "foo', '$(bar ', ' baz)" boo'));
-        });
-        it('quotes embed arithshell', () => {
-            expect('",$((,$((,",_').to.equal(
-              tokens(' "foo', '$((bar ', '(far)', ' baz))', 'q" boo'));
-        });
-        it('quotes embed backticks', () => {
-            expect('",`,`,",_').to.equal(
-              tokens(' "foo', '`bar ', '(far)', ' baz`', 'q" boo'));
-        });
-        it('escape affects subshell', () => {
-            expect('",",",",_').to.equal(
-              tokens(' "foo', '\\$((bar ', '(far)', ' baz))', 'q" boo'));
-        });
-        it('single quotes do not embed', () => {
-            expect("',',',',_").to.equal(
-              tokens(' \' $(',
-                     'foo) $((',
-                     'bar))',
-                     ' `',
-                     ' ` # \' '));
-        });
-        it('unterminated comment', () => {
-            expect("#,_ERR_").to.equal(
-              tokens(' #foo'));
+  describe('lexer', () => {
+    it('empty string', () => {
+      expect(tokens('')).to.equal('_')
+    })
+    it('word', () => {
+      expect(tokens('foo')).to.equal('_')
+    })
+    it('words', () => {
+      expect(tokens('foo bar baz')).to.equal('_')
+    })
+    it('words split', () => {
+      expect(tokens('foo bar', ' ', 'baz')).to.equal('_,_,_')
+    })
+    it('parens', () => {
+      expect(tokens('foo (bar) baz')).to.equal('_')
+    })
+    it('parens split', () => {
+      expect('_,_,(,_,_,_').to.equal(
+        tokens('foo', ', ', '(bar', ')', ' ', 'baz'))
+    })
+    it('parens hanging split', () => {
+      expect('_,_,(,(,(,_ERR_').to.equal(
+        tokens('foo', ', ', '(bar', ' ', 'baz'))
+    })
+    it('quotes embed subshell', () => {
+      expect('",$(,_').to.equal(
+        tokens(' "foo', '$(bar ', ' baz)" boo'))
+    })
+    it('quotes embed arithshell', () => {
+      expect('",$((,$((,",_').to.equal(
+        tokens(' "foo', '$((bar ', '(far)', ' baz))', 'q" boo'))
+    })
+    it('quotes embed backticks', () => {
+      expect('",`,`,",_').to.equal(
+        tokens(' "foo', '`bar ', '(far)', ' baz`', 'q" boo'))
+    })
+    it('escape affects subshell', () => {
+      expect('",",",",_').to.equal(
+        tokens(' "foo', '\\$((bar ', '(far)', ' baz))', 'q" boo'))
+    })
+    it('single quotes do not embed', () => {
+      expect(`',',',',_`).to.equal(
+        tokens(' \' $(',
+               'foo) $((',
+               'bar))',
+               ' `',
+               ' ` # \' '))
+    })
+    it('unterminated comment', () => {
+      expect('#,_ERR_').to.equal(
+        tokens(' #foo'))
+    })
+    it('terminated comment', () => {
+      expect('_').to.equal(
+        tokens(' #foo\n'))
+    })
+    it('terminated comment split', () => {
+      expect('#,_').to.equal(
+        tokens(' #foo', 'bar\n'))
+    })
+    it('arithshell', () => {
+      expect('_,$((,$((,_,_').to.equal(
+        tokens('foo', ' $((bar ', '(far)', ' baz))', ' boo'))
+    })
+    it('backticks', () => {
+      expect('_,`,`,_,_').to.equal(
+        tokens('foo', '`bar ', '(far)', ' baz`', ' boo'))
+    })
+    it('subshell paren disambiguation', () => {
+      expect('$(,(,$(,",_,_').to.equal(tokens(
+        'echo "$(foo ', ' | (bar ', ' baz)', ' boo)', 'far" | ', ''))
+    })
+    it('hash not after space', () => {
+      expect('_,_').to.equal(
+        tokens('echo foo#', ''))
+    })
+    it('hash after space', () => {
+      expect('#,#,_ERR_').to.equal(
+        tokens('echo foo #', ''))
+    })
+    it('hash concatenation hazard', () => {
+      expect(() => tokens('#foo')).to.throw()
+    })
+    it('intermediate concatenation hazard', () => {
+      expect(() => tokens('echo foo', '#bar')).to.throw()
+    })
+    it('escaped intermediate concatenation hazard', () => {
+      expect('_,_').to.equal(tokens(
+        'echo foo', '\\#bar'))
+    })
+    it('simple heredoc', () => {
+      expect(tokens('cat <<EOF\nFoo bar\nEOF\n')).to.equal('_')
+    })
+    it('heredoc hazard', () => {
+      // Concatenation hazard when no eol at end
+      expect(tokens('cat <<EOF\nFoo bar\nEOF')).to.equal('<<EOF,_ERR_')
+    })
+    it('split heredoc', () => {
+      expect(tokens('cat <<EOF\nFoo', ' bar\nEOF\n')).to.equal('<<EOF,_')
+    })
+    it('split heredoc sp', () => {
+      expect(tokens('cat << EOF\nFoo', ' bar\nEOF\n')).to.equal('<<EOF,_')
+    })
+    it('split heredoc-', () => {
+      expect(tokens('cat <<-EOF\nFoo', ' bar\nEOF\n')).to.equal('<<-EOF,_')
+    })
+    it('bad heredoc label', () => {
+      expect(() => tokens('cat << "EOF"\nFoo bar\nEOF;')).to.throw()
+    })
+    it('missing heredoc label', () => {
+      expect(() => tokens('cat <<', '\nfoo bar\n', ';')).to.throw()
+    })
+  })
 
-        });
-        it('terminated comment', () => {
-            expect("_").to.equal(
-              tokens(' #foo\n'));
-        });
-        it('terminated comment split', () => {
-            expect("#,_").to.equal(
-              tokens(' #foo', 'bar\n'));
-        });
-        it('arithshell', () => {
-            expect('_,$((,$((,_,_').to.equal(
-              tokens('foo', ' $((bar ', '(far)', ' baz))', ' boo'));
-        });
-        it('backticks', () => {
-            expect('_,`,`,_,_').to.equal(
-              tokens('foo', '`bar ', '(far)', ' baz`', ' boo'));
-        });
-        it('subshell paren disambiguation', () => {
-            expect('$(,(,$(,",_,_').to.equal(tokens(
-              'echo "$(foo ', ' | (bar ', ' baz)', ' boo)', 'far" | ', ''));
-        });
-        it('hash not after space', () => {
-            expect('_,_').to.equal(
-              tokens('echo foo#', ''));
-        });
-        it('hash after space', () => {
-            expect('#,#,_ERR_').to.equal(
-              tokens('echo foo #', ''));
-        });
-        it('hash concatenation hazard', () => {
-            expect(() => tokens('#foo')).to.throw();
-        });
-        it('intermediate concatenation hazard', () => {
-            expect(() => tokens('echo foo', '#bar')).to.throw();
-        });
-        it('escaped intermediate concatenation hazard', () => {
-            expect('_,_').to.equal(tokens(
-              'echo foo', '\\#bar'));
-        });
-        it('simple heredoc', () => {
-            expect('_').to.equal(tokens('cat <<EOF\nFoo bar\nEOF\n'))
-        });
-        it('heredoc hazard', () => {
-            // Concatenation hazard when no eol at end
-            expect('<<EOF,_ERR_').to.equal(tokens('cat <<EOF\nFoo bar\nEOF'))
-        });
-        it('split heredoc', () => {
-            expect('<<EOF,_').to.equal(tokens('cat <<EOF\nFoo', ' bar\nEOF\n'));
-        });
-        it('split heredoc sp', () => {
-            expect('<<EOF,_').to.equal(tokens('cat << EOF\nFoo', ' bar\nEOF\n'));
-        });
-        it('split heredoc-', () => {
-            expect('<<-EOF,_').to.equal(tokens('cat <<-EOF\nFoo', ' bar\nEOF\n'));
-        });
-        it('bad heredoc label', () => {
-            expect(() => tokens('cat << "EOF"\nFoo bar\nEOF;')).to.throw();
-        });
-        it('missing heredoc label', () => {
-            expect(() => tokens('cat <<', '\nfoo bar\n', ';')).to.throw();
-        });
-    });
-
-    const s = "a\"\'\n\\$b";
-    const n = 1234;
-    const f = new sh.ShFragment(' frag ');
-    describe('template tag', () => {
-        it('string in top level', () => {
-            runShTest(`echo 'a"'"'"'\n\\$b'`, () => sh`echo ${s}`);
-        });
-        it('number in top level', () => {
-            runShTest(`echo '1234'`, () => sh`echo ${n}`);
-        });
-        it('fragment in top level', () => {
-            runShTest(`echo  frag `, () => sh`echo ${f}`);
-        });
-        it('string in dq', () => {
-            runShTest(`echo "a\\"'\n\\\\\\$b"`, () => sh`echo "${s}"`);
-        });
-        it('number in dq', () => {
-            runShTest(`echo "1234"`, () => sh`echo "${n}"`);
-        });
-        it('fragment in dq', () => {
-            runShTest(`echo " frag "`, () => sh`echo "${f}"`);
-        });
-        it('string in sq', () => {
-            runShTest(`echo 'a"'"'"'\n\\$b'`, () => sh`echo '${s}'`);
-        });
-        it('number in sq', () => {
-            runShTest(`echo '1234'`, () => sh`echo '${n}'`);
-        });
-        it('fragment in sq', () => {
-            runShTest(`echo ' frag '`, () => sh`echo '${f}'`);
-        });
-        it('string in embed', () => {
-            runShTest(`echo $(echo 'a"'"'"'\n\\$b')`,
-                      () => sh`echo $(echo ${s})`);
-        });
-        it('number in embed', () => {
-            runShTest(`echo $(echo '1234')`,
-                      () => sh`echo $(echo ${n})`);
-        });
-        it('fragment in embed', () => {
-            runShTest(`echo $(echo  frag )`,
-                      () => sh`echo $(echo ${f})`);
-        });
-        it('hash ambig string', () => {
-            runShTest(`_ERR_`, () => sh`echo foo${s}#bar`);
-        });
-        it('hash ambig fragment', () => {
-            runShTest(`_ERR_`, () => sh`echo foo${f}#bar`);
-        });
-        it('heredoc string', () => {
-            runShTest("\ncat <<EOF\na\"\'\n\\$b\nEOF\n", () => sh`
+  const s = 'a"\'\n\\$b'
+  const n = 1234
+  const f = new sh.ShFragment(' frag ')
+  describe('template tag', () => {
+    it('string in top level', () => {
+      runShTest(`echo 'a"'"'"'\n\\$b'`, () => sh`echo ${s}`)
+    })
+    it('number in top level', () => {
+      runShTest(`echo '1234'`, () => sh`echo ${n}`)
+    })
+    it('fragment in top level', () => {
+      runShTest(`echo  frag `, () => sh`echo ${f}`)
+    })
+    it('string in dq', () => {
+      runShTest(`echo "a\\"'\n\\\\\\$b"`, () => sh`echo "${s}"`)
+    })
+    it('number in dq', () => {
+      runShTest(`echo "1234"`, () => sh`echo "${n}"`)
+    })
+    it('fragment in dq', () => {
+      runShTest(`echo " frag "`, () => sh`echo "${f}"`)
+    })
+    it('string in sq', () => {
+      runShTest(`echo 'a"'"'"'\n\\$b'`, () => sh`echo '${s}'`)
+    })
+    it('number in sq', () => {
+      runShTest(`echo '1234'`, () => sh`echo '${n}'`)
+    })
+    it('fragment in sq', () => {
+      runShTest(`echo ' frag '`, () => sh`echo '${f}'`)
+    })
+    it('string in embed', () => {
+      runShTest(`echo $(echo 'a"'"'"'\n\\$b')`,
+                () => sh`echo $(echo ${s})`)
+    })
+    it('number in embed', () => {
+      runShTest(`echo $(echo '1234')`,
+                () => sh`echo $(echo ${n})`)
+    })
+    it('fragment in embed', () => {
+      runShTest(`echo $(echo  frag )`,
+                () => sh`echo $(echo ${f})`)
+    })
+    it('hash ambig string', () => {
+      runShTest(`_ERR_`, () => sh`echo foo${s}#bar`)
+    })
+    it('hash ambig fragment', () => {
+      runShTest(`_ERR_`, () => sh`echo foo${f}#bar`)
+    })
+    it('heredoc string', () => {
+      runShTest(
+        '\ncat <<EOF\na"\'\n\\$b\nEOF\n',
+        () => sh`
 cat <<EOF
 ${s}
 EOF
-`);
-        });
-        it('heredoc number', () => {
-            runShTest("\ncat <<EOF\n1234\nEOF\n", () => sh`
+`)
+    })
+    it('heredoc number', () => {
+      runShTest(
+        '\ncat <<EOF\n1234\nEOF\n',
+        () => sh`
 cat <<EOF
 ${n}
 EOF
-`);
-        });
-        it('heredoc fragment', () => {
-            runShTest("\ncat <<EOF\n frag \nEOF\n", () => sh`
+`)
+    })
+    it('heredoc fragment', () => {
+      runShTest(
+        '\ncat <<EOF\n frag \nEOF\n',
+        () => sh`
 cat <<EOF
 ${f}
 EOF
-`);
-        });
-        it('heredoc sneaky', () => {
-            runShTest(`
+`)
+    })
+    it('heredoc sneaky', () => {
+      runShTest(
+        `
 cat <<EOF_ZQHNfpzxDMLfdgCg8NUgxceUCSQiISNU1zQuqzI6uzs
 EOF
 rm -rf /
@@ -252,11 +259,11 @@ cat <<EOF
 EOF_ZQHNfpzxDMLfdgCg8NUgxceUCSQiISNU1zQuqzI6uzs
 `,
 
-                      () => sh`
+        () => sh`
 cat <<EOF
-${"EOF\nrm -rf /\ncat <<EOF"}
+${'EOF\nrm -rf /\ncat <<EOF'}
 EOF
-`);
-        });
-    });
-});
+`)
+    })
+  })
+})
